bf71ca0bdfdf82cf3a0fbd8579fc6b7e4fb9c272
kyuing <kyykyu000@gmail.com>
2022-08-02 19:45:18 +0100

    07-what-is-jquery-image-uploader, 08-JS-syntax

---
+++ 07-what-is-jquery-image-uploader/js/my.js
@@ -0,0 +1,58 @@
+function readURL(input) {
+  if (input.files && input.files[0]) {
+
+    let reader = new FileReader();
+   
+    reader.onload = function(e) {
+
+      $('.image-upload-wrap').hide();
+      $('.file-upload-image').attr('src', e.target.result);
+      $('.file-upload-content').show();
+    
+    };
+    reader.readAsDataURL(input.files[0]);
+  } 
+}
+
+/**********************************************************
+ // The following is the same code as above.
+ // but I added some of the console.log() at appropriate lines
+ // so that you can see what's going on 
+ // when the function is executed 
+
+ // plz use crtl + / to enable or disable comments
+ 
+function readURL(input) {
+  if (input.files && input.files[0]) {
+
+    console.log(input);
+    console.log(input.files);
+    console.log(input.files[0]);
+    let reader = new FileReader();
+   
+    
+    reader.onload = function(e) {
+
+      $('.image-upload-wrap').hide();
+      $('.file-upload-image').attr('src', e.target.result);
+      console.log(e.target.result);
+      $('.file-upload-content').show();
+    
+    };
+    reader.readAsDataURL(input.files[0]);
+    
+  } 
+}
+*************************************************************/
+
+
+/** 
+File upload input
+Copyright (c) 2022 by Aaron Vanston (https://codepen.io/aaronvanston/pen/yNYOXR)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
---
+++ 08-JS-syntax/array.md
@@ -0,0 +1,13 @@
+# array in JS
+
+```javascript
+const web = ['html', 'css', 'javascript'];
+    
+// quick note  
+// you can also create an instance of an array object     
+// const web = new Array('html', 'css', 'javascript');
+
+console.log(web[0]);  // html
+console.log(web[1]);  // css
+console.log(web[2]);  // javascript
+```
